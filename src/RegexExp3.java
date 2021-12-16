bd1724dc5926876ea9f6bd91e994b2c0c9c97e60
HP <shubhi1488@gmail.com>
2021-12-16 07:42:25 +0530

    metacharacters

--- src/RegexExp3.java
+++ src/RegexExp3.java
@@ -6,6 +6,7 @@ public class RegexExp3 {
         System.out.println(Pattern.matches("\\d","abc"));//false as it is valid for digits only[0-9]
         System.out.println(Pattern.matches("\\d","1"));//true
         System.out.println(Pattern.matches("\\D","abc"));//false bcz more than one character
-        System.out.println(Pattern.matches("\\D","m"));
+        System.out.println(Pattern.matches("\\D","m"));//true as single character
+        System.out.println(Pattern.matches("\\D*","mark"));//true as it is valid for zero ormore than zero times
     }
 }
