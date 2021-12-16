361f8c3efd99e9b459b89408f1f795b5bb6c2f52
HP <shubhi1488@gmail.com>
2021-12-16 07:40:58 +0530

    metacharacters

---
+++ src/RegexExp3.java
@@ -0,0 +1,11 @@
+//Regular expression for metacharacters
+import java.util.regex.Pattern;
+
+public class RegexExp3 {
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches("\\d","abc"));//false as it is valid for digits only[0-9]
+        System.out.println(Pattern.matches("\\d","1"));//true
+        System.out.println(Pattern.matches("\\D","abc"));//false bcz more than one character
+        System.out.println(Pattern.matches("\\D","m"));
+    }
+}
